2c4e0947ef03a350d86a59319bd758ecd4b1be91
SSHAHEEN73 <alshaheen_5232@icloud.com>
2025-07-24 14:07:25 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,15 +2,11 @@ from extensions import db
 
 class Investment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    metal_type = db.Column(db.String(10), nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    metal_type = db.Column(db.String(50), nullable=False)
+    purchase_price = db.Column(db.Float, nullable=False)
 
 class MetalPrice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    metal_type = db.Column(db.String(50), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-
-class ProfitScenario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    scenario_name = db.Column(db.String(100), nullable=False)
-    expected_profit = db.Column(db.Float, nullable=False)
+    gold = db.Column(db.Float, nullable=False)
+    silver = db.Column(db.Float, nullable=False)
