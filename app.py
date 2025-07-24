b9171911e5fa177bb40d15a769e83db126ee5ad6
SSHAHEEN73 <alshaheen_5232@icloud.com>
2025-07-24 14:06:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask
 from extensions import db
 from routes import main_bp
-import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SECRET_KEY'] = 'supersecret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    app.register_blueprint(main_bp)
 
     with app.app_context():
         db.create_all()
 
+    app.register_blueprint(main_bp)
+
     return app
 
-# خلي app موجود على مستوى الملف
 app = create_app()
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
